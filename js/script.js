29736c670669608ee4bfaf3fe1c0ee694be57444
unknown <akamat173@gmail.com>
2019-07-24 17:39:21 +0530

    local storage bug[undefined/alert-box] solved, Checkbox added

--- js/script.js
+++ js/script.js
@@ -1,37 +1,37 @@
 
 
-var btn = document.querySelector('#threefuns');
+var btn = document.querySelector('#threefuncs');
 btn.addEventListener('click',fetch);
 btn.addEventListener('click',checkGender);
-btn.addEventListener('click',validate);
+btn.addEventListener('click',valid);
 btn.addEventListener('click',store);
 
-function fetch(){
-    var name=document.getElementById("name").value;
+function fetch() {
+  	var fname=document.getElementById("fname").value;
     var lname=document.getElementById("lname").value;
-    email=document.getElementById("email").value;
+    email= document.getElementById("email").value;
     num=document.getElementById("num").value;
-	console.log("Name: "+name+"\n last name: "+lname+"\n email: "+email+"\n num: "+num);
+	console.log("Name: "+fname+"\n last name: "+lname+"\n email: "+email+"\n num: "+num);
 
 }
 
-function checkGender(){				
+function checkGender() {				
 	if(document.getElementById('radioMale').checked)
 	console.log("You have selected Male.");
 	else
 	console.log("You have selected Female.");
 }
 
-function validate(){
+function valid(){
 	if (this.checked) 
 		console.log("");
 	else 
-		alert("the console box is checked");
+		console.log("the console box is checked");
 }
 
 function store(){
-	localStorage.setItem("name", name.value);
-	localStorage.setItem("lname", lname.value);
-	localStorage.setItem("email", email.value);
-	localStorage.setItem("num", num.value);
+	localStorage.setItem('fname', fname.value);
+	localStorage.setItem('lname', lname.value);
+	localStorage.setItem('mail',JSON.stringify(email));
+	localStorage.setItem('num', JSON.stringify(num));
 }
