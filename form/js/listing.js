a40a80b1415024aaab19df859d54ab6a68d063a6
chaturthi <akamat173@gmail.com>
2019-08-05 18:32:28 +0530

    Jquery dynamic list creation

--- form/js/listing.js
+++ form/js/listing.js
@@ -2,7 +2,12 @@ $(document).ready(function () {
     var display = JSON.parse(localStorage.getItem('UserData'));
 
     for (i = 0; i < display.length; i++) {
-        let liItem = "<li>" + display[i].firstname + "</li>";
+        let liItem = "<li>" + "<span>" + display[i].firstname + "</span>" + " " + "<span>" + display[i].email + "</span>" + "<i class="+ "material-icons" + "></i>" + "</li>";
         $("ol").append(liItem);
+        $("li").addClass("listitem");
+        $("li").attr('id', i);
+        $("li").append('<i class="material-icons">' + "delete" +'</i>');
+
     }
 });
+
