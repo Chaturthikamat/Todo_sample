81a1208d0236eaa455a7ff74056f36d6bf992d10
chaturthi <akamat173@gmail.com>
2019-08-08 17:13:57 +0530

    Userdata list- Multiple Users Array and css

--- form/js/listing.js
+++ form/js/listing.js
@@ -1,13 +1,12 @@
-$(document).ready(function () {
+
+$(document).ready(function ()  {
+
     var display = JSON.parse(localStorage.getItem('UserData'));
-
-    for (i = 0; i < display.length; i++) {
-        let liItem = "<li>" + "<span>" + display[i].firstname + "</span>" + " " + "<span>" + display[i].email + "</span>" + "<i class="+ "material-icons" + "></i>" + "</li>";
-        $("ol").append(liItem);
-        $("li").addClass("listitem");
-        $("li").attr('id', i);
-        $("li").append('<i class="material-icons">' + "delete" +'</i>');
-
+        for (i = 0; i < display.length; i++) {
+            let liItem = "<li>" + "<span>" + display[i].firstname + "</span>" + " " + "<span>" + display[i].email + "</span>" + "<i class=" + "material-icons" + "></i>" + "</li>";
+            $("ol").append(liItem);
+            $("li").addClass("listitem");
+            $("li").attr('id', i);
     }
+    $("li").append('<i class="material-icons">' + "delete" + '</i>');
 });
-
--- form/js/service.js
+++ form/js/service.js
@@ -1,13 +1,21 @@
+//function to add data in the array
 function AddData(formData) {
-	let data=[{
-	firstname : formData.fname,
-	lastname : formData.lname,
-	email: formData.email,
-	PhoneNumber : formData.mobnum
-	}];
-	
-	//local storage
-	window.localStorage.setItem('UserData', JSON.stringify(data));
-	console.log(data);
-    
+	let users = getData()
+	let data = {
+		firstname: formData.fname,
+		lastname: formData.lname,
+		email: formData.email,
+		PhoneNumber: formData.mobnum
+	};
+	if (data !== null) {
+		users.push(data);
+		window.localStorage.setItem('UserData', JSON.stringify(users));
+	}
+	console.log(JSON.parse(localStorage.getItem('UserData')));
+}
+
+//function to get data in the array and display in listing
+function getData() {
+	var display = JSON.parse(localStorage.getItem('UserData'));
+	return display;
 }
