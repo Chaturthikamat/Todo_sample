739c5c03eb0dcfc791c42317edfb27c6a21ecd15
chaturthi <akamat173@gmail.com>
2019-08-09 13:01:59 +0530

    The null pointer for the array issue resolved, css enhancements

--- form/js/service.js
+++ form/js/service.js
@@ -1,5 +1,6 @@
 //function to add data in the array
 function AddData(formData) {
+	console.log(getData());
 	let users = getData()
 	let data = {
 		firstname: formData.fname,
@@ -10,12 +11,12 @@ function AddData(formData) {
 	if (data !== null) {
 		users.push(data);
 		window.localStorage.setItem('UserData', JSON.stringify(users));
+		console.log(JSON.parse(localStorage.getItem('UserData')));
 	}
-	console.log(JSON.parse(localStorage.getItem('UserData')));
 }
 
 //function to get data in the array and display in listing
 function getData() {
-	var display = JSON.parse(localStorage.getItem('UserData'));
+	var display = JSON.parse(localStorage.getItem('UserData'))|| [] ;
 	return display;
 }
