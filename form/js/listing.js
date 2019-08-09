f91756e4a2838a1f96ba1c5cd66d4c7852003e85
chaturthi <akamat173@gmail.com>
2019-08-09 18:14:26 +0530

    The AJAX call to GET the data

--- form/js/listing.js
+++ form/js/listing.js
@@ -1,12 +1,12 @@
 
-$(document).ready(function ()  {
+// $(document).ready(function ()  {
 
-    var display = JSON.parse(localStorage.getItem('UserData'));
-        for (i = 0; i < display.length; i++) {
-            let liItem = "<li>" + "<span>" + display[i].firstname + "</span>" + " " + "<span>" + display[i].email + "</span>" + "<i class=" + "material-icons" + "></i>" + "</li>";
-            $("ol").append(liItem);
-            $("li").addClass("listitem");
-            $("li").attr('id', i);
-    }
-    $("li").append('<i class="material-icons">' + "delete" + '</i>');
-});
+//     var display = JSON.parse(localStorage.getItem('UserData'));
+//         for (i = 0; i < display.length; i++) {
+//             let liItem = "<li>" + "<span>" + display[i].firstname + "</span>" + " " + "<span>" + display[i].email + "</span>" + "<i class=" + "material-icons" + "></i>" + "</li>";
+//             $("ol").append(liItem);
+//             $("li").addClass("listitem");
+//             $("li").attr('id', i);
+//     }
+//     $("li").append('<i class="material-icons">' + "delete" + '</i>');
+// });
--- form/js/service.js
+++ form/js/service.js
@@ -1,22 +1,45 @@
-//function to add data in the array
-function AddData(formData) {
-	console.log(getData());
-	let users = getData()
-	let data = {
-		firstname: formData.fname,
-		lastname: formData.lname,
-		email: formData.email,
-		PhoneNumber: formData.mobnum
-	};
-	if (data !== null) {
-		users.push(data);
-		window.localStorage.setItem('UserData', JSON.stringify(users));
-		console.log(JSON.parse(localStorage.getItem('UserData')));
-	}
-}
+ $(function GetData(){
+	 var $data= $('#dataDisplay');
+	 debugger;
+	$.ajax({
+		type:'GET',
+		url:'https://jsonplaceholder.typicode.com/users',
+		success: function(data) {
+		console.log('success',data);
+			$.each(data, function(i, users) {
+				$data.append('<li>Name: '+ users.name +'<br> Email: '+ users.email +' </li>');
+				
+			});
+			$("li").append('<i class="material-icons">' + "delete" + '</i>');
+		}  
 
-//function to get data in the array and display in listing
-function getData() {
-	var display = JSON.parse(localStorage.getItem('UserData'))|| [] ;
-	return display;
-}
+	});
+
+ });
+
+
+
+
+// //function to add data in the array
+// function AddData(formData) {
+// 	console.log(getData());
+// 	let users = getData()
+// 	let data = {
+// 		firstname: formData.fname,
+// 		lastname: formData.lname,
+// 		email: formData.email,
+// 		PhoneNumber: formData.mobnum
+// 	};
+// 	if (data !== null) {
+// 		users.push(data);
+// 		window.localStorage.setItem('UserData', JSON.stringify(users));
+// 		console.log(JSON.parse(localStorage.getItem('UserData')));
+// 	}
+// }
+
+// //function to get data in the array and display in listing
+// function getData() {
+// 	var display = JSON.parse(localStorage.getItem('UserData'))|| [] ;
+// 	return display;
+// }
+
