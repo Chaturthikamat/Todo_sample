f3a63e4d347baab95b4ba749ee20c04696fe6c29
chaturthi <akamat173@gmail.com>
2019-08-16 14:19:50 +0530

    The delete and update error

--- form/js/service.js
+++ form/js/service.js
@@ -4,31 +4,113 @@
  	debugger;
  	$.ajax({
  		type: 'GET',
- 		url: 'https://jsonplaceholder.typicode.com/users',
+ 		url: 'https://jsonplaceholder.typicode.com/users/1',
  		success: function (data) {
  			console.log('success', data);
  			$.each(data, function (i, users) {
- 				$data.append('<li>' + '<span>' + users.name + '</span>' + '<span>' + users.email + '</span>' + ' </li>');
+ 				$data.append('<li>' + '<span>' + users.name + '</span>' + '<br> <span>' + users.email + '</span>' + ' </li>');
  			});
+ 			//adds li dynamically 
  			$("li").append('<i class="material-icons delete">' + "delete" + '</i>');
  			$("li").append('<i class="material-icons edit">' + "edit" + '</i>');
  		}
  	});
-
  	alert('success');
  });
 
- //delete data
- $('#dataDisplay').on('click', '.delete', function () {
- 	$(this).parent().remove();
+ //Updating(editing) selected user
+ $(function updateData() {
+ 	$(".edit").on("click", function () {
+ 		var $data = $('#dataDisplay');
+ 		//var parentlistElement = $(this).parent(); // points to li
+ 		$.ajax({
+ 			method: "PUT",
+ 			url: "https://jsonplaceholder.typicode.com/posts/1",
+ 			dataType: 'json',
+ 			data: {
+ 				"type": "main"
+ 			},
+ 			cache: false,
+ 			success: function (data) {
+ 				// update the specific list item
+ 				$(this).parent().html(object.values.join("-"));
+ 				//parentListElement.data.attr('contenteditable', 'true');
+ 			}
+ 		})
+ 	})
+ })
+
+
+ // Deleting selected user
+ $(function deleteUser() {
+ 	$(".delete").on("click", function () {
+ 		var id = $('#dataDisplay').id;
+ 		var data = $(this).parent();
+ 		$.ajax({
+ 			method: "DELETE",
+ 			url: 'https://jsonplaceholder.typicode.com/posts/1',
+ 			data: {
+ 				name: users.name,
+ 				email: users.email
+ 			},
+ 			success: function (data) {
+ 				data.remove(data, id);
+ 			}
+ 		})
+ 	});
  });
 
- //edit data
- $('#dataDisplay').on('click', '.edit', function () {
- 	//window.location.href = "../index.html";
- 	$(this).parent().attr('contenteditable', 'true');
- 	//$("li").append('<i class="material-icons save">' + "save" + '</i>');
- });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
