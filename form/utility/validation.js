7a83eecbe2107563df1a72e01d6307689d949530
chaturthi <akamat173@gmail.com>
2019-08-27 18:30:15 +0530

    the validation part bug fixed 2

--- form/utility/validation.js
+++ form/utility/validation.js
@@ -5,24 +5,24 @@ function validation(data) {
     let lname = data.lname;
     let email = data.email;
     let mobnum = data.mobnum;
+    debugger;
 
-    if (fname === "" || !isNaN(fname)) {
+    //the regex
+    var numbers = /^[0-9]+$/;
+    var letters = /^[A-Za-z]+$/;
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    //the validation
+    if (fname == "" || !fname.match(letters)) {
         $("#fname_error")[0].innerHTML = "** Error: Please enter the username, do not enter number";
         validateValue = false;
-    }
-
-    if (lname === "" || !isNaN(lname)) {
-        $("#name_error")[0].innerHTML = "** Error: Please enter the username, do not enter number";
+    } else if (lname == "" || !lname.match(letters)) {
+        $("#name_error")[0].innerHTML = "** Error: Please enter the lastname correctly";
         validateValue = false;
-    }
-
-    if (email === "") {
-        $("#useremail")[0].innerHTML = "** Error: Please enter the email Id in valid format";
+    } else if (email == "" || !email.match(mailformat)) {
+        $("#uemail")[0].innerHTML = "** Error: Please enter the email Id in valid format";
         validateValue = false;
-    }
-
-
-    if (mobnum === "" || mobnum.length != 10 || isNaN(mobnum)) {
+    } else if (mobnum == "" || mobnum.length != 10 || !mobnum.match(numbers)) {
         $("#usernum")[0].innerHTML = "** Error: Please enter the 10-digit number";
         validateValue = false;
     } else {
